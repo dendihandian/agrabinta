9abf7ac81d19036f04dc11f38bce27505a1973a5
Dendi Handian <handian.dendi@gmail.com>
2020-10-25 16:35:37 +0700

    fix example nav

--- src/components/AppExample/AppExample.js
+++ src/components/AppExample/AppExample.js
@@ -32,58 +32,58 @@ const AppExample = () => {
             <nav className="flex justify-start p-2 bg-blue-800">
                 {/* {examples.map((example, i) => {
                     return (
-                        <div key={i} className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                        <div key={i} className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                             <Link to={example.path}>{example.label}</Link>
                         </div>
                     )
                 })} */}
 
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/simple">Simple Map & Marker</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/events">Events</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/viewport">Viewport</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/vector-layers">Vector Layers</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/other-layers">Other Layers</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/tooltip">Tooltip</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/zoom-control">Zoom Control</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/layers-control">Layers Control</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/pane">Pane</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/draggable">Draggable</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/bounds">Bounds</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/custom-component">Custom Component</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/animate">Animate</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/wms-tile">WMS Tile</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/video-overlay">Video Overlay</Link>
                 </div>
-                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded flex items-center justify-center text-center">
                     <Link to="/examples/custom-icons">Custom Icons</Link>
                 </div>
             </nav>
