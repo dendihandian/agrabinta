2f0db228034b6d7077b7563903bcbb1f78dd33d5
Dendi Handian <handian.dendi@gmail.com>
2020-10-25 16:32:01 +0700

    more examples

--- src/components/AppExample/AppExample.js
+++ src/components/AppExample/AppExample.js
@@ -8,6 +8,15 @@ import VectorLayersExample from './VectorLayersExample';
 import OtherLayersExample from './OtherLayersExample';
 import TooltipExample from './TooltipExample';
 import ZoomControlExample from './ZoomControlExample';
+import LayersControlExample from './LayersControlExample';
+import PaneExample from './PaneExample';
+import DraggableExample from './DraggableExample';
+import BoundsExample from './BoundsExample';
+import CustomComponentExample from './CustomComponentExample';
+import AnimateExample from './AnimateExample';
+import WMSTileLayerExample from './WMSTileLayerExample';
+import VideoOverlayExample from './VideoOverlayExample';
+import CustomIconsExample from './CustomIconsExample';
 
 const AppExample = () => {
     // const examples = [
@@ -50,6 +59,33 @@ const AppExample = () => {
                 <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
                     <Link to="/examples/zoom-control">Zoom Control</Link>
                 </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/layers-control">Layers Control</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/pane">Pane</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/draggable">Draggable</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/bounds">Bounds</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/custom-component">Custom Component</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/animate">Animate</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/wms-tile">WMS Tile</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/video-overlay">Video Overlay</Link>
+                </div>
+                <div className="mr-2 bg-yellow-500 py-1 px-2 text-xs rounded">
+                    <Link to="/examples/custom-icons">Custom Icons</Link>
+                </div>
             </nav>
 
             <div id="section-example-inner">
@@ -66,6 +102,15 @@ const AppExample = () => {
                     <Route path="/examples/other-layers" component={OtherLayersExample} />
                     <Route path="/examples/tooltip" component={TooltipExample} />
                     <Route path="/examples/zoom-control" component={ZoomControlExample} />
+                    <Route path="/examples/layers-control" component={LayersControlExample} />
+                    <Route path="/examples/pane" component={PaneExample} />
+                    <Route path="/examples/draggable" component={DraggableExample} />
+                    <Route path="/examples/bounds" component={BoundsExample} />
+                    <Route path="/examples/custom-component" component={CustomComponentExample} />
+                    <Route path="/examples/animate" component={AnimateExample} />
+                    <Route path="/examples/wms-tile" component={WMSTileLayerExample} />
+                    <Route path="/examples/video-overlay" component={VideoOverlayExample} />
+                    <Route path="/examples/custom-icons" component={CustomIconsExample} />
                 </Switch>
             </div>
         </section>
