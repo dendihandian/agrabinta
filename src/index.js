a19a0b44efd6da2c8fe284cb84caa1c4ca785fe7
Dendi Handian <dendihandian@gmail.com>
2020-12-16 09:27:32 +0700

    implement tailwind as instructed in https://tailwindcss.com/docs/guides/create-react-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
-// css styles
-// TODO: the css was working, but icons doesn't. find a way to fix broken icons.
-// NOTE: we temporary apply leaflet cdn in the public/index.html for the time being.
-// import 'leaflet/dist/images/marker-icon-2x.png';
-// import 'leaflet/dist/images/marker-icon.png';
-// import 'leaflet/dist/images/marker-shadow.png';
-// import 'leaflet/dist/leaflet.css';
-import './styles/css/app.css';
 
 ReactDOM.render(
   <React.StrictMode>
