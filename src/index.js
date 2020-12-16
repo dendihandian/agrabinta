fb821abf2eb6f52c7fe26c346e07d87c75b47175
Dendi Handian <dendihandian@gmail.com>
2020-12-16 14:22:39 +0700

    use leaflet style from module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../node_modules/leaflet/dist/leaflet.css';
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-
 
 ReactDOM.render(
   <React.StrictMode>
