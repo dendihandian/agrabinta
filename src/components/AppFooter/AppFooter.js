1b8d722beddde49bf403818c4c9ea2f012f3d422
Dendi Handian <dendihandian@gmail.com>
2021-01-30 20:48:53 +0700

    about and footer fill

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -3,8 +3,8 @@ import React from 'react';
 const AppFooter = () => {
     const year = new Date().getFullYear()
     return (
-        <footer className="h-8 bg-gray-800 flex justify-center items-center">
-            <p className="text-sm text-gray-600">{year} &copy; Dendi Handian</p>
+        <footer className="flex items-center justify-center h-8 bg-gray-800">
+            <p className="text-sm text-gray-600 hover:text-gray-400">{year} &copy; Dendi Handian</p>
         </footer>
     )
 }
